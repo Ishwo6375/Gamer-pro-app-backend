652ea8a7fe8e82efd14220920dd3251c6fd225e8
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-10-25 21:41:15 -0400

    implemented review controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -47,5 +47,6 @@ rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
     def render_unprocessable_entity(invalid)
         render json: { error: invalid.record.errors.full_messages }, status: :unprocessable_entity
     end
-end
+
+  
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,52 @@
 class ReviewsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+
+ def index
+        @reviews = Review.all
+        render json: @reviews
+    end
+    
+    def create
+     @reviews = Review.create!(review_params)
+     render json: @reviews
+    end
+
+
+    def show
+        @review = find_review 
+        render json: @review
+    end
+
+    def update
+        @review = find_review
+        @review.update!(review_params)
+        render json: @review
+    end
+ 
+ def destroy
+  @review = find_review
+  review.destroy
+  head :no_content
+ end
+
+  private
+
+   def review_params
+    params.require(:review).permit(:content, :user_username, :user_id, :game_id)
+   end
+
+   def find_review
+      Review.find(params[:id])
+   end
+
+    def render_not_found
+        render json: { error: "Review not found"}, status: :not_found
+    end
+
+    def render_unprocessable_entity(invalid)
+        render json: { error: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+  
 end
