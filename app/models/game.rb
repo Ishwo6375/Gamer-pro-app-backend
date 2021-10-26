4e0f05a69cc8fb112107d047b5d4b9a0cc7b4825
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-10-26 09:07:55 -0400

    added dependent destroy to models

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ApplicationRecord
-has_many :reviews
-has_many :favoritelists
+has_many :reviews, dependent: :destroy
+has_many :favoritelists, dependent: :destroy
 belongs_to :genre
 has_many :like_dislikes
 has_many :users, through: :reviews 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-has_many :reviews
-has_many :favoritelists
+has_secure_password
+has_many :reviews, dependent: :destroy
+has_many :favoritelists, dependent: :destroy
 has_many :like_dislikes
 has_many :games, through: :reviews
 end
