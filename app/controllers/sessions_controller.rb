ea17a43a9c7072abfd2d6e6f5aaaf7c14ca2ee2c
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-10-31 14:39:23 -0400

    updated applicaton controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,21 @@
 class SessionsController < ApplicationController
- 
-    def login
-      @user = User.first
-      render json: @user
+    skip_before_action :authorized, only: [:create]
+
+    def create
+        byebug
+        @user = User.find_by(username: user_login_params[:username])
+        if @user && @user.authenticate(user_login_params[:password])
+          token = encode_token({ user_id: @user.id })
+          render json: { user: @user, jwt: token }, status: :accepted
+        else
+          render json: { message: 'Invalid username or password' }, status: :unauthorized
+        end
+    end
+    
+    private
+
+    def user_login_params
+        {username: params["username"], password: params["password"]}
     end
 
-    def signup
-      @user = User.last
-      render json: @user
-    end
 end
